5a0cb9c8cfe17d9862686775d408bff4e688ab6b
JennyShalai <jenny.shalai@gmail.com>
2016-05-17 13:53:33 -0400

    yo to mutable dictionary

--- ApplesAndHolidays/FISAppDelegate.m
+++ ApplesAndHolidays/FISAppDelegate.m
@@ -11,10 +11,55 @@
     return YES;
 }
 
-/**
- 
- * Implement your methods here.
- 
- */
+- (NSArray *)pickApplesFromFruits:(NSArray *)fruits {
+    NSMutableArray *appleArray = [NSMutableArray new];
+    for (NSString *fruit in fruits) {
+        if ([fruit isEqualToString:@"apple"]) {
+            [appleArray addObject:fruit];
+        }
+    }
+    return appleArray;
+}
+
+- (NSArray *)holidaysInSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    NSMutableArray *holidaysInSeason = database[season];
+    return holidaysInSeason;
+}
+
+- (NSArray *)suppliesInHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    NSMutableArray *suppliesInHoliday = database[season][holiday];
+    return suppliesInHoliday;
+}
+
+-(BOOL)holiday:(NSString *)holiday isInSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    bool isHolidayInSeason = NO;
+    NSMutableArray * holidaysInSeason = database[season][holiday];
+    if ([holidaysInSeason count] > 0) {
+        isHolidayInSeason = YES;
+    }
+    return isHolidayInSeason;
+}
+
+-(BOOL)supply:(NSString *)supply isInHoliday:( NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    bool isSupplyInHoliday = NO;
+    NSMutableArray * suppliesInHoliday = database[season][holiday];
+    if ([suppliesInHoliday containsObject:supply]) {
+        isSupplyInHoliday = YES;
+    }
+    return isSupplyInHoliday;
+}
+
+- (NSDictionary *)addHoliday:(NSString *)holiday toSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    NSMutableDictionary *databaseMutable = [database mutableCopy];
+    NSArray *value = @[];
+    [databaseMutable [season] setValue:value forKey:holiday];
+    return databaseMutable;
+}
+
+- (NSDictionary *)addSupply:(NSString *)supply toHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    NSMutableDictionary *databaseMutable = [database mutableCopy];
+    [databaseMutable [season] setValue:supply forKey:holiday];
+    return databaseMutable;
+}
 
 @end
